fef1cac1f11f610dde4b43bc8f54c86c0058b990
RICHARD S. RICHARD <richardngaye105@gmail.com>
2026-03-08 20:38:52 +0300

    Delete js directory

--- js/helpers.js
+++
@@ -1,5 +0,0 @@
-
-function byId(id){return document.getElementById(id);}
-function copyText(value){navigator.clipboard.writeText(value||'').then(()=>alert('Copied'));}
-function downloadText(filename, content, mime='text/plain;charset=utf-8'){const blob=new Blob([content],{type:mime});const a=document.createElement('a');a.href=URL.createObjectURL(blob);a.download=filename;a.click();setTimeout(()=>URL.revokeObjectURL(a.href),400);}
-function downloadCanvas(canvas, filename='image.png'){const a=document.createElement('a');a.href=canvas.toDataURL('image/png');a.download=filename;a.click();}
